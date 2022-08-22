31c4e341199c78a4d6f796d27f5889f974fca6e7
CodingWRLD <aidanhudson88@gmail.com>
2022-08-22 14:00:33 +0100

    Update Interaction.cs
    Add topmost arg. For overlay

--- SUPLauncher-Reborn/Interaction.cs
+++ SUPLauncher-Reborn/Interaction.cs
@@ -8,18 +8,21 @@ namespace SUPLauncher_Reborn
 {
     class Interaction
     {
-        public static string InputBox(string question, string title)
+        public static string InputBox(string question, string title, bool topMost = false)
         {
             
             inputbox form = new inputbox(title, question);
             form.Text = title;
+            form.TopMost = topMost;
             form.ShowDialog();
             return form.text;
         }
 
-        public static void MessageBox(string message, string title = null)
+        public static void MessageBox(string message, string title = null, bool topMost = false)
         {
+            
             msgBox form = new msgBox(message, title);
+            form.TopMost = topMost;
             form.ShowDialog();
         }
     }
